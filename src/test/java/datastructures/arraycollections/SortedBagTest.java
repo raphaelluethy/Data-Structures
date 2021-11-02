56cb95fb0b04614338e0769e40b374857d22e695
Lucifer Uchiha <rowla101@gmail.com>
2021-11-02 10:53:52 +0100

    Implemented UnsortedBag

---
+++ src/test/java/datastructures/arraycollections/SortedBagTest.java
@@ -0,0 +1,33 @@
+package datastructures.arraycollections;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import datastructures.arraycollections.general.AbstractBagTest;
+import datastructures.arraycollections.general.ElementExistenceChecker;
+
+public class SortedBagTest extends AbstractBagTest {
+	@Override
+	protected <T extends Comparable<? super T>> AbstractArrayCollection<T> createCollection(int size) {
+		return new SortedBag<T>(size);
+	}
+
+	@Override
+	protected Integer[] getExpectedOrderFor(Integer[] values) {
+		List<Integer> list = new LinkedList<Integer>(Arrays.asList(values));
+		Collections.sort(list);
+		Integer[] elements = new Integer[list.size()];
+		int index = 0;
+		for (Integer element : list) {
+			elements[index++] = element;
+		}
+		return elements;
+	}
+
+	@Override
+	protected void checkElementExistence(Comparable<Integer>[] numbers) {
+		ElementExistenceChecker.inGivenOrder(Arrays.copyOf(numbers, numbers.length), bag);
+	}
+}
---
+++ src/test/java/datastructures/arraycollections/general/Elem.java
@@ -0,0 +1,28 @@
+package datastructures.arraycollections.general;
+
+final class Elem implements Comparable<Elem> {
+	private final int value;
+
+	public Elem(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public int hashCode() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return this == obj || obj != null && getClass() == obj.getClass() && ((Elem)obj).value == value;
+	}
+
+	@Override
+	public int compareTo(Elem o) {
+		return value - o.value;
+	}
+}
