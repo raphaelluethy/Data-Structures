e046c598f0fe96ee19e5e7df36e03e1f12ae532d
Lucifer Uchiha <rowla101@gmail.com>
2021-11-02 13:34:10 +0100

    Implemented SortedSet

--- src/main/java/datastructures/arraycollections/SortedSet.java
+++ src/main/java/datastructures/arraycollections/SortedSet.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class SortedSet<E extends Comparable<? super E>> extends AbstractArrayCollection<E> implements Set<E> {
     public static final int DEFAULT_CAPACITY = 100;
-    private E[] data;
+    private final E[] data;
+    private int size = 0;
 
     public SortedSet() {
         this(DEFAULT_CAPACITY);
     }
 
-    @SuppressWarnings("unchecked")
     public SortedSet(int capacity) {
         data = (E[]) new Comparable[capacity];
     }
@@ -19,27 +19,61 @@ public class SortedSet<E extends Comparable<? super E>> extends AbstractArrayCol
     public static void main(String[] args) {
         SortedSet<Integer> bag = new SortedSet<Integer>();
         bag.add(2);
-        bag.add(2);
         bag.add(1);
+        bag.add(2);
+        System.out.println(Arrays.toString(bag.toArray()));
+        System.out.println("SortedSet is sorted and does not allows the same value multiple times.");
+        System.out.println("Bag contains 1: " + bag.contains(1));
+        bag.remove(1);
         System.out.println(Arrays.toString(bag.toArray()));
     }
 
     @Override
     public boolean add(E e) {
-        // TODO implement unless collection shall be immutable
-        throw new UnsupportedOperationException();
+        if (e == null) throw new NullPointerException();
+        int index = getIndexOf(e);
+        if(index >= 0) return false;
+        if (size == data.length) throw new IllegalStateException("Collection is full!");
+        index = -index - 1; // index becomes insertionPoint
+        shiftRight(index);
+        data[index] = e;
+        size++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        // TODO implement unless collection shall be immutable
-        throw new UnsupportedOperationException();
+        int index = getIndexOf(o);
+        if (index >= 0) { // Exists
+            shiftLeft(index);
+            data[--size] = null; // Memory Leak cleanup
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean contains(Object o) {
-        // TODO must be implemented
-        throw new UnsupportedOperationException();
+        return getIndexOf(o) >= 0;
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+    }
+
+    private void shiftRight(int index) {
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+    }
+
+    private int getIndexOf(Object o) {
+        if (o == null) throw new NullPointerException();
+        // Arrays.binarySearch returns index if found, otherwise (-(insertPosition) -1)
+        // Where insertionPoint is the index of the first element larger then o.
+        return Arrays.binarySearch(data, 0, size, o);
     }
 
     @Override
@@ -49,7 +83,6 @@ public class SortedSet<E extends Comparable<? super E>> extends AbstractArrayCol
 
     @Override
     public int size() {
-        // TODO must be implemented
-        return 0;
+        return size;
     }
 }
