958f5b50e6dc2ca7860d2fe5a34080723fea03ca
Lucifer Uchiha <rowla101@gmail.com>
2021-11-02 23:19:11 +0100

    Implemented MyStack

--- src/main/java/datastructures/list/mysol/stack/MyStack.java
+++ src/main/java/datastructures/list/mysol/stack/MyStack.java
@@ -1,41 +1,46 @@
 package datastructures.list.mysol.stack;
 
 import datastructures.list.exercise.stack.IStack;
+import datastructures.list.mysol.linkedlist.MyLinkedList;
+
+import java.util.EmptyStackException;
 
 public class MyStack<E> implements IStack<E> {
-	@Override
-	public E pop() {
-		// TODO implement this operation
-		throw new UnsupportedOperationException();
-	}
 
-	@Override
-	public E push(E elem) {
-		// TODO implement this operation
-		throw new UnsupportedOperationException();
-	}
+    MyLinkedList<E> list = new MyLinkedList<>();
 
-	@Override
-	public E peek() {
-		// TODO implement this operation
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public E pop() {
+        if (empty()) throw new EmptyStackException();
+        return list.remove(list.size() - 1);
+    }
 
-	@Override
-	public boolean empty() {
-		// TODO implement this operation
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public E push(E elem) {
+        list.add(elem);
+        return elem;
+    }
 
-	public static void main(String[] args) {
-		MyStack<Integer> stack = new MyStack<Integer>();
-		System.out.println("Pushing numbers to stack (0 to 9)");
-		for (int i = 0; i < 10; i++) {
-			stack.push(i);
-		}
-		System.out.println("Pop numbers from stack");
-		while (!stack.empty()) {
-			System.out.println(stack.pop());
-		}
-	}
+    @Override
+    public E peek() {
+        if(empty()) throw new EmptyStackException();
+        return list.get(list.size() - 1);
+    }
+
+    @Override
+    public boolean empty() {
+        return list.size() < 1;
+    }
+
+    public static void main(String[] args) {
+        MyStack<Integer> stack = new MyStack<Integer>();
+        System.out.println("Pushing numbers to stack (0 to 9)");
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        System.out.println("Pop numbers from stack");
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+    }
 }
